307ceea09182e3080f797a1c94bf2559e9d7a6db
Phelippe <lipe.otsugua17@gmail.com>
2021-01-02 00:11:03 -0300

    Adicionadas novas fontes ao projeto: OpenSans e Quicksand. O transaction.user foi deletado e seu conteudo transformou-se em um stateful widget no main. Foi refatorada o codigo do formulario, fechando o modal automaticamente, gerando teclado numerico e usando o ListView com seu builder que processa os elementos a medida que sao renderizados, evitando consumo de memoria.

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,93 @@
 import 'package:flutter/material.dart';
-import 'components/transaction_user.dart';
+import 'components/transaction_form.dart';
+import 'components/transaction_list.dart';
+import 'models/transaction.dart';
+import 'dart:math';
 
 main() => runApp(ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MyHomePage());
+    return MaterialApp(
+      home: MyHomePage(),
+      theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.blue,
+          fontFamily: 'Quicksand',
+          textTheme: ThemeData.light().textTheme.copyWith(
+                headline6: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+          appBarTheme: AppBarTheme(
+            textTheme: ThemeData.light().textTheme.copyWith(
+                  headline6: TextStyle(
+                      fontFamily: 'OpenSans',
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+          )),
+    );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _transactions = [
+    Transaction(
+        id: 't1',
+        title: 'Jaqueta de couro',
+        value: 300.50,
+        date: DateTime.now()),
+    Transaction(
+      id: 't2',
+      title: 'Boné',
+      value: 70,
+      date: DateTime.now(),
+    )
+  ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+
+    Navigator.of(context).pop();
+  }
+
+  _openTransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (_) {
+        return TransactionForm(_addTransaction);
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Despesas Pessoais'),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () => _openTransactionFormModal(context))
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -29,10 +101,14 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            TransactionUser(),
+            TransactionList(_transactions),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () => _openTransactionFormModal(context)),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
