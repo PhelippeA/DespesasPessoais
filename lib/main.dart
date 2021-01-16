bda0d2669a21f6e989ee53687cfd1fb427688e1e
Phelippe <lipe.otsugua17@gmail.com>
2021-01-16 19:13:02 -0300

    apagando testes de lifecycle no main

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
+class _MyHomePageState extends State<MyHomePage> {
   bool _showChart = false;
   final List<Transaction> _transactions = [
     Transaction(
@@ -65,23 +65,6 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
       date: DateTime.now().subtract(Duration(days: 3)),
     )
   ];
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addObserver(this);
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    print(state);
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    WidgetsBinding.instance.removeObserver(this);
-  }
 
   List<Transaction> get _recentTransactions {
     return _transactions
